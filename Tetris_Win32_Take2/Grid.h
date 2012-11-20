5915ed8e51a1bdd04bc90a05954ce19663fb582b
Charles <psion.king@gmail.com>
2012-11-20 18:50:30 -0500

    header file comments

--- Tetris_Win32_Take2/Grid.h
+++ Tetris_Win32_Take2/Grid.h
@@ -1,4 +1,22 @@
 //Grid.h
+/*
+	The Grid class is the simpler part of my alogrithm. It's built
+	around a single character array, which the Tetro class alters.
+	The classes's functions deal with entire rows of the grid at
+	once, checking for completed rows and clearing them (or
+	declaring game over) as needed.
+
+	The grid also serves as a guide for the main file's onPaint
+	function, with (0,0) in the bottom left corner.
+	
+	Ex:
+	4|
+	3|
+	2|
+	1|
+	0|_ _ _ _ _
+	  0 1 2 3 4
+*/
 #ifndef GRID_H
 #define GRID_H
 //#include <cstring>
--- Tetris_Win32_Take2/Tetro.h
+++ Tetris_Win32_Take2/Tetro.h
@@ -1,16 +1,34 @@
 //Tetro.h
+/*
+	The Tetro class is the part of the algorithm responsible for most
+	of my headaches. It behaves a little bit like a machine making
+	its way across the grid class's array, altering the data as it
+	goes to indicate its current position. Once it reaches the bottom,
+	it makes a final alteration, changes shape, and moves back into
+	the queue area to the side of the main playing field.
+
+	I know the way it's implemented looks odd, and the mountain of
+	minor calculations aredefinitely a pain to deal with, but it's
+	working, and this way I can easily break the tetrominoes apart
+	when lines get completed and cleared by the grid.
+*/
 #ifndef TETRO_H
 #define TETRO_H
 #include "grid.h"
 class TETRO
 {
 private:
-	int m_x1, m_x2, m_x3, m_x4, m_y1, m_y2, m_y3, m_y4, m_rot;
+	//four sets of coordinates:
+	int m_x1, m_x2, m_x3, m_x4;
+	int m_y1, m_y2, m_y3, m_y4;
+	//position tetro is rotated in (1-4)
+	int m_rot;
+	//shape of tetro
 	char m_type;
 public:
 	TETRO(GRID * grid);
 	void reset(GRID * grid);
-	void dropIn(GRID * grid, TETRO * spare1, TETRO * spare2, TETRO * spare3);
+	void dropIn(GRID * grid, TETRO * spare1, TETRO * spare2, TETRO * spare3);	//not currently used
 	void dropIn2(GRID * grid);
 	void rotateLeft(GRID * grid);
 	void rotateRight(GRID * grid);
@@ -19,7 +37,7 @@ public:
 	void moveRight(GRID * grid);
 	void slamDown(GRID * grid, int * score, int * lines, int * level);
 	void lockIn(GRID * grid, int * score, int * lines, int * level);
-	void shiftUp(void);
+	void shiftUp(void);															//not currently used
 	void printStats(void);
 	//void writeStats(FILE * pFile);
 };
